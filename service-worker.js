6bbd95b8aad08ae7107b657c887d713e61b25c8e
anumayank <anumayank1999@gmail.com>
2024-05-03 06:15:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tryApp/precache-manifest.f072d0928e622dee3be648687e79d361.js"
+  "/tryApp/precache-manifest.36547dffbfbd61dabd994239ffb9f2a5.js"
 );
 
 self.addEventListener('message', (event) => {
