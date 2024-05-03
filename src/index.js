684c988bfb3010aacc5d153bb3c5b365b6ee4d05
anumayank <anumayank1999@gmail.com>
2024-05-03 07:36:47 -0700

    tryApp

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar/>
+    <App/>
     <BrowserRouter basename={'/tryApp'}> 
     {/*<NavbarHash/><HashRouter >*/}
     <Routes>
