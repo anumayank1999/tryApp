1943e32be3052f6a01983ee9b772256f49f6dbca
anumayank <anumayank1999@gmail.com>
2024-05-03 06:18:47 -0700

    tryApp

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar/>
-    <BrowserRouter basename='/tryApp'>
+    <BrowserRouter >
     <Routes>
     <Route path='/tryApp' element={<Home/>} />
     <Route path='/tryApp/Form' element={<App/>} />
