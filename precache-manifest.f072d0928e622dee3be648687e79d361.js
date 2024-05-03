f4b0f979670cc2cdc45bd0aa2ade445af53cea8c
anumayank <anumayank1999@gmail.com>
2024-05-03 06:18:32 -0700

    Updates

--- precache-manifest.f072d0928e622dee3be648687e79d361.js
+++ precache-manifest.f072d0928e622dee3be648687e79d361.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f5ff3a727b7a763c48a90e4cf23198f",
+    "revision": "5f7035b4ad6ecd8b5f5b14e35f3dfb9f",
     "url": "/tryApp/index.html"
   },
   {
-    "revision": "6bedab0d4141a6a92a52",
+    "revision": "a5e3168c065b00049394",
     "url": "/tryApp/static/css/main.16e5667b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tryApp/static/js/3.18a8d937.chunk.js"
   },
   {
-    "revision": "6bedab0d4141a6a92a52",
-    "url": "/tryApp/static/js/main.61fb561f.chunk.js"
+    "revision": "a5e3168c065b00049394",
+    "url": "/tryApp/static/js/main.2b5041f5.chunk.js"
   },
   {
     "revision": "bce5e649d0c9acba61a6",
