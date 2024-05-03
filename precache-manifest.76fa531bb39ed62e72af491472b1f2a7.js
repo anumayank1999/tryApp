5d003dd159754f8eb5db9cbf4bee16bd7419b151
anumayank <anumayank1999@gmail.com>
2024-05-03 05:37:47 -0700

    Updates

---
+++ precache-manifest.76fa531bb39ed62e72af491472b1f2a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edb3a5a6ba0f82721736f1c77df52f4",
+    "url": "/tryApp/index.html"
+  },
+  {
+    "revision": "7030d3d6be0093fed2f2",
+    "url": "/tryApp/static/css/main.16e5667b.chunk.css"
+  },
+  {
+    "revision": "04b392f826115902a274",
+    "url": "/tryApp/static/js/2.12bc9ba7.chunk.js"
+  },
+  {
+    "revision": "ac090904937ff15ba3f7",
+    "url": "/tryApp/static/js/3.18a8d937.chunk.js"
+  },
+  {
+    "revision": "7030d3d6be0093fed2f2",
+    "url": "/tryApp/static/js/main.ff5339f5.chunk.js"
+  },
+  {
+    "revision": "bce5e649d0c9acba61a6",
+    "url": "/tryApp/static/js/runtime~main.74f56a24.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tryApp/static/media/logo.06e73328.svg"
+  }
+]);
